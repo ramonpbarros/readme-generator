88edac6759317463734706d537dd31f23124116b
Sorrab <ramonpbarros@gmail.com>
2020-04-02 17:42:45 -0700

    created files and npm init set up

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO: import fs, path and inquirer modules
+
+// TODO: import api and generateMarkdown modules from ./utils/
+
+// TODO: Add inquirer question objects to questions array. This should
+// include all the necessary questions for the user.
+// Example question:
+// {
+//   type: "input",
+//   name: "github",
+//   message: "What is your GitHub username?"
+// }
+const questions = [];
+
+// TODO: Write function to synchronously write data in the
+// current working directory to file named for the fileName parameter.
+// The data parameter is the text to write to the file.
+function writeToFile(fileName, data) {
+}
+
+// TODO: Use inquirer to prompt the user for each question in the
+// questions array. Then call api.getUser to fetch the user profile
+// data from GitHub. Finally generate the markdown and use writeToFile
+// to create the README.md file.
+function init() {
+
+}
+
+init();
